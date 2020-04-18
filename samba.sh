be48e6b682afb65185464b0a5997a24cf5838877
docop <60154829+docop@users.noreply.github.com>
2020-04-18 13:44:31 -0400

    Update samba.sh

--- samba.sh
+++ samba.sh
@@ -2,7 +2,20 @@
 # Samba install script for CentOS 7 by lucandroid70@gmail.com
 yum install -y samba samba-client samba-common
 
+#Tout commande pr faire un serveur Samba, pr windows. en mode Anonyme et avec user!
+#dns Win10 : fait Run: met : \\64.137.22.225  et ca ouvre direct un win explorer avec 2 folder
+#un folder : Anonymous et un Secure. Pr ouvrir le Secure: ca va demander un psw. ici: user: cloud
+# le mode script ne marche pas. Mais toute les commande ci-dessous. c OK
+
+sudo systemctl enable firewalld
+#si une new VM centos7 min : faire apres: yum update   / reboot.
+# au reboot, faire: sudo firewall-cmd --state  , ca va dire : running. 
+
 mv /etc/samba/smb.conf /etc/samba/_smb.conf
+
+#ici ca fait un nouveau fichier et on colle cela dedans.
+#juste a faire: vi
+vi /etc/samba/smb.conf
 
 cat >> /etc/samba/smb.conf << EOF
 [global]
@@ -29,10 +42,11 @@ writable = yes
 guest ok = yes
 read only = no
 EOF
+#ne pas mettre EOF.   juste :wq
 
-useradd luke
+useradd cloud
 groupadd smbshared
-usermod -a -G smbshared luke
+usermod -a -G smbshared cloud
 
 mkdir -p /opt/shared
 chmod -R 0777 /opt/shared
@@ -41,7 +55,7 @@ chcon -t samba_share_t /opt/shared
 
 mkdir /opt/secure
 chmod -R 0777 /opt/secure
-chown -R luke:smbshared /opt/secure
+chown -R cloud:smbshared /opt/secure
 chcon -t samba_share_t /opt/secure
 
 firewall-cmd --permanent --zone=public --add-service=samba
@@ -52,5 +66,7 @@ systemctl enable nmb.service
 systemctl restart smb.service
 systemctl restart nmb.service
 
-# insert password for user 'luke'
-smbpasswd -a luke
+# insert password for user 'cloud'
+smbpasswd -a cloud
+#fin.  Le serveur roule et tout est accessible depuis win10 ou netboot!
+#pr aller sur le folder : Secure, ca va demanderle user/pwd. met cloud et pw: celui que l'on a mit.
